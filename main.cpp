bef46a0c76227b1fd0743600334cc2c6205d95a6
Mafupa <ryankarera@gmail.com>
2023-09-28 12:46:55 +0200

    Added glad and simple test window

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+	if (key == GLFW_KEY_ESCAPE  && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GLFW_TRUE);
+	else
+		printf("key : %d was pressed\n", key);
+}
+
+
 int main(void){
-	std::cout << "Hello World!" << std::endl;
-	return (0);
+	GLFWwindow *window;
+
+	if (!glfwInit())
+		exit(EXIT_FAILURE);
+	window = glfwCreateWindow(640, 480, "Test window", NULL, NULL);
+	if (!window)
+		exit(EXIT_FAILURE);
+	
+	glfwSetKeyCallback(window, key_callback);
+	glfwMakeContextCurrent(window);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		//
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	exit(EXIT_SUCCESS);
 }
